303a67a85d7b0b49860cd63d54a0ca56f04bbe8c
root <root@OliverPC>
2023-12-25 22:35:16 +0100

    Changed positioning to be in the centre, changed font colors, changed the button backgrounds

--- menu.py
+++ menu.py
@@ -29,10 +29,10 @@ def play():
         screen.fill("black")
 
         play_text = get_font(45).render("This is the PLAY screen.", True, "White")
-        play_rect = play_text.get_rect(center=(500, 250))
+        play_rect = play_text.get_rect(center=(640, 250))
         screen.blit(play_text, play_rect)
 
-        play_back = Button(image=None, pos=(500, 450), text_input="BACK",
+        play_back = Button(image=None, pos=(640, 450), text_input="BACK",
                            font=get_font(75), base_color="White", hover_color="Red")
         play_back.color_change(play_mouse_position)
         play_back.update(screen)
@@ -59,10 +59,10 @@ def settings():
         screen.fill("black")
 
         settings_text = get_font(45).render("this is the SETTINGS screen.", True, "White")
-        settings_rect = settings_text.get_rect(center=(500, 250))
+        settings_rect = settings_text.get_rect(center=(640, 250))
         screen.blit(settings_text, settings_rect)
 
-        settings_back = Button(image=None, pos=(500, 450), text_input="BACK", font=get_font(75), base_color="White",
+        settings_back = Button(image=None, pos=(640, 450), text_input="BACK", font=get_font(75), base_color="White",
                                hover_color="Red")
         settings_back.color_change(settings_mouse_position)
         settings_back.update(screen)
@@ -84,15 +84,16 @@ def main_menu():
         screen.blit(background, (0, 0))
         menu_mouse_pos = pygame.mouse.get_pos()
 
-        menu_text = get_font(100).render("MAIN MENU", True, "Red")
+        menu_text = get_font(100).render("", True, "Red")
+        # leftover code. Kept in case I go back or graphics fail and I add indexing
         menu_rect = menu_text.get_rect(center=(640, 150))
-        play_button = Button(image=pygame.image.load("assets/untitled_design.png"), pos=(640, 250), text_input="PLAY",
-                             font=get_font(75), base_color="Red", hover_color="Blue")
-        settings_button = Button(image=pygame.image.load("assets/untitled_design.png"), pos=(640, 400),
+        play_button = Button(image=pygame.image.load("assets/Button_border.png"), pos=(640, 270), text_input="PLAY",
+                             font=get_font(45), base_color="White", hover_color="#F51B14")
+        settings_button = Button(image=pygame.image.load("assets/Button_border.png"), pos=(640, 400),
                                  text_input="SETTINGS",
-                                 font=get_font(75), base_color="Red", hover_color="Blue")
-        quit_button = Button(image=pygame.image.load("assets/untitled_design.png"), pos=(640, 550), text_input="QUIT",
-                             font=get_font(75), base_color="Red", hover_color="Blue")
+                                 font=get_font(45), base_color="White", hover_color="#F51B14")
+        quit_button = Button(image=pygame.image.load("assets/Button_border.png"), pos=(640, 540), text_input="QUIT",
+                             font=get_font(45), base_color="White", hover_color="#F51B14")
 
         screen.blit(menu_text, menu_rect)
 
