c67261722a486fbf00284465e6f7d358323f5e66
root <root@OliverPC>
2024-01-13 18:03:23 +0100

    Adding player sprite and making sure it sizes correctly

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 import pygame
 class Player:
-    def __init__(self, screen_width, screen_height, player_sprite="assets/"):
+    def __init__(self, screen_width, screen_height, size=100, player_sprite="assets/Player_sprite.png"):
+        self.size = size
         self.image = pygame.image.load(player_sprite)
+        self.image = pygame.transform.scale(self.image, (size, size))
         self.rect = self.image.get_rect()
         self.screen_width = screen_width
         self.screen_height = screen_height
@@ -12,11 +14,12 @@ class Player:
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.rect.y -= speed if self.rect.y > 0 else 0  # Move up
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            self.rect.y += speed if self.rect.y < self.screen_height - self.rect.height else 0  # Move down
+            self.rect.y += speed if self.rect.y < self.screen_height - self.size else 0  # Move down
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rect.x -= speed if self.rect.x > 0 else 0  # Move left
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            self.rect.x += speed if self.rect.x < self.screen_width - self.rect.width else 0  # Move right
+            self.rect.x += speed if self.rect.x < self.screen_width - self.size else 0  # Move right
+
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
