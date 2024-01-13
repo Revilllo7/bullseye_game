431396c7d2c35b4d3db975a17ca0f714e1f2d1fe
root <root@OliverPC>
2024-01-13 17:36:00 +0100

    Added the player.py which is responsible for initialising itself and controlling player movement

---
+++ player.py
@@ -0,0 +1,22 @@
+import pygame
+class Player:
+    def __init__(self, screen_width, screen_height, player_sprite="assets/"):
+        self.image = pygame.image.load(player_sprite)
+        self.rect = self.image.get_rect()
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.rect.center = (screen_width // 2, screen_height // 2)
+
+    def move(self, keys):
+        speed = 5
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.rect.y -= speed if self.rect.y > 0 else 0  # Move up
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.rect.y += speed if self.rect.y < self.screen_height - self.rect.height else 0  # Move down
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.rect.x -= speed if self.rect.x > 0 else 0  # Move left
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rect.x += speed if self.rect.x < self.screen_width - self.rect.width else 0  # Move right
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
