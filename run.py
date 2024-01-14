3be2cdda90e47838c16b47449872377511e4dff8
root <root@OliverPC>
2024-01-14 16:22:22 +0100

    added player shoots arrow call that draws on the screen

--- run.py
+++ run.py
@@ -1,25 +1,17 @@
-from player import *
+import pygame
+from player import Player
 import sys
-
 
 pygame.init()
 
-# Set up the screen
 res = (1280, 720)
 screen = pygame.display.set_mode(res)
 pygame.display.set_caption("Bullseye_game")
 
-ground_color = (0, 255, 0)
-def draw_ground():
-    pygame.draw.line(screen, ground_color, (0, 100), (1280, 100))
+player = Player(screen)
 
-# Initialize the player
-player = Player(res[0], res[1])
-
-# Set up clock for controlling the frame rate
 clock = pygame.time.Clock()
 
-# Main game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,10 +22,8 @@ while True:
     player.move(keys)
 
     screen.fill((255, 255, 255))
-    player.draw(screen)
-
-    if keys[pygame.K_q]:
-        player.shoot(screen)
+    player.draw()
+    player.shoot_arrow()
 
     pygame.display.flip()
     clock.tick(60)
